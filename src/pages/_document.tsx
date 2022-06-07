2a831c19e24e1df5bf8261cda9fcb7f63da0619e
jamiegyoung <jamiegyoung22@gmail.com>
2022-06-08 00:15:14 +0100

    feat: add webmanifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -28,7 +28,7 @@ export default function Document() {
           sizes="16x16"
           href="/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/site.webmanifest.json" />
       </Head>
       <body>
         <Main />
