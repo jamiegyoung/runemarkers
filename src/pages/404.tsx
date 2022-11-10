786dbcc0f83b91aaadeed3eddf36fe3f62819569
jamiegyoung <jamiegyoung22@gmail.com>
2022-11-10 20:31:11 +0000

    style(404): fix button text decoration

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -14,7 +14,12 @@ export default function FourOhFour() {
       <NavBar />
       <div className={styles.container}>
         <h1 className={styles.header}>404</h1>
-        <Link href="/">
+        <Link
+          href="/"
+          style={{
+            textDecoration: `none`,
+          }}
+        >
           <span className={styles.home}>Go Home</span>
         </Link>
       </div>
