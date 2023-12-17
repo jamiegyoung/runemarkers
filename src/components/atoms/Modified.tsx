6beb5ca88765ef9a35648d4b01ed775cba23ae11
jamiegyoung <jamiegyoung22@gmail.com>
2023-12-17 03:27:34 +0000

    fix(modified): fix modified tooltip clipping

--- src/components/atoms/Modified.tsx
+++ src/components/atoms/Modified.tsx
@@ -4,7 +4,9 @@ export default function Modified({ children }: { children: string }) {
   return (
     <span className={styles.tooltip}>
       (modified)
-      <span className={styles.tooltiptext}>{children}</span>
+      <span className={styles.tooltipWrapper}>
+        <span className={styles.tooltipText}>{children}</span>
+      </span>
     </span>
   );
 }
