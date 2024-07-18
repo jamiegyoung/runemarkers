253cae3835bcfd0e9e162c860e0be695d2c34767
jamiegyoung <jamiegyoung22@gmail.com>
2024-07-18 23:54:34 +0100

    fix(builder): remove duplicate error logging

--- cmd/build/main.go
+++ cmd/build/main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/jamiegyoung/runemarkers-go/internal/args"
 	"github.com/jamiegyoung/runemarkers-go/internal/builder"
+	"github.com/jamiegyoung/runemarkers-go/internal/logger"
 )
+
+var log = logger.New("build")
 
 func main() {
 	argsWithoutProg := os.Args[1:]
 	skipThumbs := args.HasArg(argsWithoutProg, "--skip-thumbs") || args.HasArg(argsWithoutProg, "-st")
 
-	builder.Build(skipThumbs)
+	err := builder.Build(skipThumbs)
+	if err != nil {
+		log(fmt.Sprintf("An error occured: %v", err))
+		log(err.Error())
+	}
+
 }
--- internal/builder/builder.go
+++ internal/builder/builder.go
@@ -1,7 +1,6 @@
 package builder
 
 import (
-	"fmt"
 	"os"
 
 	"github.com/jamiegyoung/runemarkers-go/internal/api"
@@ -26,7 +25,6 @@ func Build(skipThumbs bool) error {
 	if _, err := os.Stat(destination); os.IsNotExist(err) {
 		err := os.Mkdir(destination, 0755)
 		if err != nil {
-			logErr("making destination file", err)
 			return err
 		}
 	}
@@ -34,57 +32,46 @@ func Build(skipThumbs bool) error {
 
 	ents, err := entities.ReadAllEntities()
 	if err != nil {
-		logErr("reading entities", err)
 		return err
 	}
 
 	log("Generating entities")
 	err = api.Generate(ents)
 	if err != nil {
-		logErr("generating entities", err)
 		return err
 	}
 
 	log("Collecting thumbnails")
-	thumbnails.Collect(ents, destination, skipThumbs)
+	err = thumbnails.Collect(ents, destination, skipThumbs)
+	if err != nil {
+		return err
+	}
 
 	log("Generating pages")
 	err = pages.GeneratePages(destination, ents)
 	if err != nil {
-		logErr("generating pages", err)
 		return err
 	}
 
 	err = entitypages.GeneratePages(destination, ents)
 	if err != nil {
-		logErr("generating entity pages", err)
 		return err
 	}
 
 	err = libs.Copy(destination + "/js")
 	if err != nil {
-		logErr("copying libraries", err)
 		return err
 	}
 
 	err = assets.Copy(destination + "/assets")
 	if err != nil {
-		logErr("copying assets", err)
 		return err
 	}
 
 	err = robots.Copy(destination)
 	if err != nil {
-		logErr("copying robots.txt", err)
 		return err
 	}
 
 	return nil
-}
-
-func logErr(ctx string, err error) {
-	if err != nil {
-		log(fmt.Sprintf("An error occured when %v!", ctx))
-		log(err.Error())
-	}
 }
