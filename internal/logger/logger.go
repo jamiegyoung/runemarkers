4daa2601451f1ed6e7c4ff3ae39d1c3c3bbd0482
jamiegyoung <jamiegyoung22@gmail.com>
2024-07-19 03:12:16 +0100

    fix(logger): show full value

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -8,6 +8,6 @@ import (
 func New(prefix string) func(string) {
 	return func(message string) {
 		now := time.Now().Format("15:04:05")
-		fmt.Printf("[%v %v] %v\n", now, prefix, message)
+		fmt.Printf("[%+v %+v] %+v\n", now, prefix, message)
 	}
 }
