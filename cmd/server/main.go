1a04fbb26c92ffeedbca76be42ceef3def8bcf17
jamiegyoung <jamiegyoung@proton.me>
2025-02-26 01:54:06 +0000

    fix(server): add check for build failures and log if so

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"github.com/jamiegyoung/runemarkers/internal/builder"
 	"github.com/jamiegyoung/runemarkers/internal/server"
+	"github.com/jamiegyoung/runemarkers/internal/logger"
 )
 
+var log = logger.New("server")
+
 func main() {
-	builder.Build(false)
+	err := builder.Build(false)
+	if err != nil {
+		log("An error occured: " + err.Error())
+		return
+	}
+
 	server.Start()
 }
